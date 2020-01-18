e5348b24464360b165cfdfd935a008c660a56cc3
fengzhizi715 <fengzhizi715@126.com>
2020-01-18 21:03:51 +0800

    update build.gradle

---
+++ app/src/main/java/com/safframework/netdiagnose/activity/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.safframework.netdiagnose.activity
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.netdiagnose.activity.MainActivity
+ * @author: Tony Shen
+ * @date: 2020-01-18 20:44
+ * @version: V1.0 <描述当前版本功能>
+ */
---
+++ app/src/main/java/com/safframework/netdiagnose/app/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.safframework.netdiagnose.app
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.netdiagnose.app.BaseActivity
+ * @author: Tony Shen
+ * @date: 2020-01-18 20:44
+ * @version: V1.0 <描述当前版本功能>
+ */
+open class BaseActivity : AppCompatActivity() {
+
+
+}
