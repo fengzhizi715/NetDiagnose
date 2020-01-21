91b385423a5a930416913f312e34d4abaf5e1809
fengzhizi715 <fengzhizi715@126.com>
2020-01-22 01:53:27 +0800

    add NeettyClient

---
+++ app/src/main/java/com/safframework/netdiagnose/connect/ws/NettyClient.kt
@@ -0,0 +1,67 @@
+package com.safframework.netdiagnose.connect.ws
+
+import java.net.URI
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.netdiagnose.connect.ws.NettyClient
+ * @author: Tony Shen
+ * @date: 2020-01-06 12:39
+ * @version: V1.0 <描述当前版本功能>
+ */
+object NettyClient {
+
+    private var client: NettyWebSocketClient? = null
+
+    private var url = ""
+
+    private val mListener = object : NettyClientListener<String> {
+
+        override fun onMessageResponseClient(msg: String) {
+
+            handlerMsg(msg)
+        }
+    }
+
+    @JvmStatic
+    fun connect() {
+
+        stop()
+
+        try {
+            val serverURI = URI(url)
+            client = NettyWebSocketClient(serverURI, mListener).apply {
+                connect()
+            }
+
+        } catch (e: Exception) {
+
+            e.printStackTrace()
+        }
+    }
+
+    @JvmStatic
+    fun getClient(): NettyWebSocketClient? = client
+
+    /**
+     * 客户端停止
+     */
+    @JvmStatic
+    fun stop() {
+
+        client?.let {
+
+            if (!it.isClosed) {
+                it.close()
+            }
+        }
+    }
+
+    /**
+     * 处理服务端收到的信息
+     */
+    private fun handlerMsg(msg: String) {
+
+    }
+}
