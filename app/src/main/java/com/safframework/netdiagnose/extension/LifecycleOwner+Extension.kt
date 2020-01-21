1dc4de246e7fe1c9a4d60e6ef9bdc45c6a032dfb
fengzhizi715 <fengzhizi715@126.com>
2020-01-21 14:25:20 +0800

    add extension

---
+++ app/src/main/java/com/safframework/netdiagnose/extension/LifecycleOwner+Extension.kt
@@ -0,0 +1,20 @@
+package com.safframework.netdiagnose.extension
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.netdiagnose.extension.`LifecycleOwner+Extension`
+ * @author: Tony Shen
+ * @date: 2020-01-21 14:21
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>, action: (t: T) -> Unit) {
+
+    liveData.observe(this, Observer {
+        it?.let { t -> action(t) }
+    })
+}
