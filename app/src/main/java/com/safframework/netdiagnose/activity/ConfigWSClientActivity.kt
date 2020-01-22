e7aec8029a5e45566adae661219475e8ab55822f
fengzhizi715 <fengzhizi715@126.com>
2020-01-22 14:10:37 +0800

    add ConfigWSClientActivity

---
+++ app/src/main/java/com/safframework/netdiagnose/activity/ConfigWSClientActivity.kt
@@ -0,0 +1,23 @@
+package com.safframework.netdiagnose.activity
+
+import com.safframework.netdiagnose.app.BaseActivity
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.netdiagnose.activity.ConfigWSClientActivity
+ * @author: Tony Shen
+ * @date: 2020-01-22 14:08
+ * @version: V1.0 <描述当前版本功能>
+ */
+class ConfigWSClientActivity :BaseActivity(){
+
+    override fun layoutId(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
