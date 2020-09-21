24512ac8bdaf6b2fe566c01f0aa2c2d254c90274
fengzhizi715 <fengzhizi715@126.com>
2020-09-21 20:53:25 +0800

    refactor NetHelper.kt

--- app/src/main/java/com/safframework/netdiagnose/utils/NetHelper.kt
+++ app/src/main/java/com/safframework/netdiagnose/utils/NetHelper.kt
@@ -1,7 +1,11 @@
 package com.safframework.netdiagnose.utils
 
+import cn.netdiscovery.command.Appender
+import cn.netdiscovery.command.CommandBuilder
+import cn.netdiscovery.command.CommandExecutor
 import com.safframework.log.L
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
 
@@ -17,59 +21,29 @@ object NetHelper {
 
     val TAG = "NetHelper"
 
-    fun ping(timeout: Long = 30000): Boolean {
+    fun ping(): Boolean {
         try {
             val ip = "www.baidu.com"// ping 的地址，可以换成任何一种可靠的外网
-            val status = executeCommand("ping -c 1 -w 100 $ip", timeout)
+//            val status = executeCommand("ping -c 1 -w 100 $ip", timeout)
+
+            val cmd = CommandBuilder("ping").withArgs("-c","1","$ip").build()
+
+            val status = CommandExecutor.executeSync(cmd, null,5, TimeUnit.SECONDS,object : Appender {
+                override fun appendStdText(text: String) {
+                }
+
+                override fun appendErrText(text: String) {
+                }
+
+            }).getExecutionResult().exitValue()
+
+            L.d("status = $status")
+
             // ping的状态
             return status == 0
         } catch (e: Exception) {
             L.d(TAG, "ping失败：" + e.message)
-        } finally {
         }
         return false
-    }
-
-    /**
-     * 运行一个外部命令，返回状态.若超过指定的超时时间，抛出TimeoutException
-     *
-     * @param command
-     * @param timeout
-     * @return
-     * @throws IOException
-     * @throws InterruptedException
-     * @throws TimeoutException
-     */
-    @Throws(IOException::class, InterruptedException::class, TimeoutException::class)
-    fun executeCommand(command: String, timeout: Long): Int? {
-        val process = Runtime.getRuntime().exec(command)
-        val worker = Worker(process)
-        worker.start()
-        try {
-            worker.join(timeout)
-            return if (worker.exit != null) {
-                worker.exit
-            } else {
-                throw TimeoutException("连接超时")
-            }
-        } catch (ex: InterruptedException) {
-            worker.interrupt()
-            Thread.currentThread().interrupt()
-            throw ex
-        } finally {
-            process.destroy()
-        }
-    }
-
-    private class Worker constructor(private val process: Process) : Thread() {
-        var exit: Int? = null
-
-        override fun run() {
-            try {
-                exit = process.waitFor()
-            } catch (ignore: InterruptedException) {
-                L.d(TAG, ignore.message)
-            }
-        }
     }
 }
